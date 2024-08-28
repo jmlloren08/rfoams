c6c960d4d6365b6018fdf4afbe6625fd38f1e4b1
jmlloren08 <65296830+jmlloren08@users.noreply.github.com>
2024-08-28 09:48:15 +0800

    update composer

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->string('roles');
+            $table->string('roles')->nullable();
             $table->timestamps();
         });
 
