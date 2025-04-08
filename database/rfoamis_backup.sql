68b8994644d5c84e9e3be22ab22a6eca896d9f99
jmlloren08 <65296830+jmlloren08@users.noreply.github.com>
2025-04-08 11:59:43 +0800

    Delete database directory

--- database/rfoamis_backup.sql
+++
@@ -1,6 +0,0 @@
-LOAD DATA INFILE 'listofda.csv'
-INTO TABLE department_agencies
-FIELDS TERMINATED BY ',' ENCLOSED BY '"'
-LINES TERMINATED BY '\r\n'
-IGNORE 1 LINES
-(department_agencies, address, contact_number);
